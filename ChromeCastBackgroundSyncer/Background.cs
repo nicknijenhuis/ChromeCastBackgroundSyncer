9735eb1bdcfa210261d2d5d5a6d8758b11623c39
Nick Nijenhuis <nick.nijenhuis@qnh.nl>
2017-10-03 11:00:35 +0200

    Chrome cast background syncer

---
+++ ChromeCastBackgroundSyncer/Background.cs
@@ -0,0 +1,9 @@
+﻿namespace ChromeCastBackgroundSyncer
+{
+    class Background
+    {
+        public string Url { get; set; }
+
+        public string Author { get; set; }
+    }
+}
--- ChromeCastBackgroundSyncer/Program.cs
+++ ChromeCastBackgroundSyncer/Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ChromeCastBackgroundSyncer
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var settings = new Settings();
+            configuration.Bind(settings);
+
+            var backgrounds = await GetBackgroundsInformation(settings);
+            Directory.CreateDirectory(settings.Folder);
+            DownloadBackgrounds(backgrounds, settings);
+            Console.ReadLine();
         }
+
+        private static async Task<IEnumerable<Background>> GetBackgroundsInformation(Settings settings)
+        {
+            IEnumerable<Background> backgrounds;
+            using (var httpClient = new HttpClient())
+            {
+                var json = await httpClient.GetStringAsync(settings.JsonUrl);
+
+                backgrounds = JsonConvert.DeserializeObject<List<Background>>(json);
+                Console.WriteLine("JSON downloaded");
+            }
+            return backgrounds;
+        }
+
+        private static void DownloadBackgrounds(IEnumerable<Background> backgrounds, Settings settings)
+        {
+            foreach (var background in backgrounds)
+            {
+                var uri = new Uri(background.Url);
+
+                string filename = Path.GetFileName(uri.Segments.Last());
+                string filePath = Path.Combine(settings.Folder, filename);
+
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine($"{filePath} already exists.");
+                    continue;
+                }
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.DownloadFileAsync(uri, filePath);
+                    webClient.DownloadFileCompleted += (sender, eventArgs) => Console.WriteLine($"{filePath} downloaded.");
+                }
+            }
+        }
+    }
+
+    public class Settings
+    {
+        public string JsonUrl { get; set; }
+
+        public string Folder { get; set; }
     }
 }
